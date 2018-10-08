f8df66226874cf536054a84284ca9177b88e6d99
dstevens <dstevens@DSTEVENS-DESK.uk.oracle.com>
2018-10-08 14:52:55 +0100

    Updates

---
+++ precache-manifest.3689fbb0e36cdcaade7dbf03eb7cc8fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/osvc-links/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "733bada63309f851554b",
+    "url": "/osvc-links/static/js/runtime~main.733bada6.js"
+  },
+  {
+    "revision": "4b5018717d0ee400bf1b",
+    "url": "/osvc-links/static/js/main.4b501871.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/osvc-links/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "4b5018717d0ee400bf1b",
+    "url": "/osvc-links/static/css/main.4ebbbc45.chunk.css"
+  },
+  {
+    "revision": "d3b96f38f5a526105c3865a428b1bafd",
+    "url": "/osvc-links/index.html"
+  }
+];
