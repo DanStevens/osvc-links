8f1df8d50690220ed2cda456c72f2b343d545d13
Dan Stevens <dan.stevens@oracle.com>
2018-10-10 16:29:34 +0100

    Updates

---
+++ precache-manifest.1e26d15b292045d484ec7b68e34bc4c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "733bada63309f851554b",
+    "url": "/osvc-links/static/js/runtime~main.733bada6.js"
+  },
+  {
+    "revision": "32e61d8d057e812640be",
+    "url": "/osvc-links/static/js/main.32e61d8d.chunk.js"
+  },
+  {
+    "revision": "8ae1694990e798cafe63",
+    "url": "/osvc-links/static/js/1.8ae16949.chunk.js"
+  },
+  {
+    "revision": "32e61d8d057e812640be",
+    "url": "/osvc-links/static/css/main.996c08d4.chunk.css"
+  },
+  {
+    "revision": "a3053ccb94a5571b03f79caff8b0d640",
+    "url": "/osvc-links/index.html"
+  }
+];
